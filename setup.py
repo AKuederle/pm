d79fe533ecafd0d18d1c7d9b140e3319eadcd38c
Arne Küderle <a.kuederle@gmail.com>
2020-02-02 22:39:23 +0100

    All scripts are just simple scripts

--- setup.py
+++ setup.py
@@ -9,11 +9,5 @@ setup(
     author='arne',
     author_email='',
     description='A simple commandline project manager',
-    scripts=['project_manager/scripts/p'],
-    entry_points={
-        'console_scripts': [
-            '_p=project_manager.scripts.p:main'
-        ],
-
-    }
+    scripts=['project_manager/scripts/p', 'project_manager/scripts/_p.py']
 )
